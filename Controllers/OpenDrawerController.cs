bf5e7fb91251359e360e532dc6c9b1221c37caf8
mrt <iquixe@gmail.com>
2024-07-24 21:18:24 +0300

    Add try catch on post open drawer api

--- Controllers/OpenDrawerController.cs
+++ Controllers/OpenDrawerController.cs
@@ -12,15 +12,19 @@ namespace PrintingApi.Controllers {
 
     [HttpPost(Name = "PostOpenDrawer")]
     public dynamic Post(string printerName) {
-      const string ESC = "\u001B";
-      const string p = "\u0070";
-      const string m = "\u0000";
-      const string t1 = "\u0025";
-      const string t2 = "\u0250";
-      const string openTillCommand = ESC + p + m + t1 + t2;
+      try {
+        const string ESC = "\u001B";
+        const string p = "\u0070";
+        const string m = "\u0000";
+        const string t1 = "\u0025";
+        const string t2 = "\u0250";
+        const string openTillCommand = ESC + p + m + t1 + t2;
 
-      RawPrinterHelper.SendStringToPrinter(printerName, openTillCommand);
-      return Ok();
+        RawPrinterHelper.SendStringToPrinter(printerName, openTillCommand);
+        return Ok();
+      } catch (Exception e) {
+        return StatusCode(StatusCodes.Status500InternalServerError, e);
+      }
     }
   }
 }
