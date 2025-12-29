5df9a6ddb3404038cb9e6a1f17bee08e7b550149
mrt <motifyee@gmail.com>
2025-12-29 06:56:55 +0200

    refactor: Move `print_stamp.png` to `TemplatePrinting` and refactor embedded resource handling to use a generic `Resources<T>` class with an `Assets` enum for type-safe access.

--- PrintingLibrary/EmbeddedResourcesUtils/EmbeddedResource.cs
+++ PrintingLibrary/EmbeddedResourcesUtils/EmbeddedResource.cs
@@ -1,18 +1,16 @@
+using System.ComponentModel.DataAnnotations;
 using System.Reflection;
 
 namespace PrintingLibrary.EmbeddedResourcesUtils;
 
-internal static class InternalResources
-{
-  public static Stream? GetStream(string relativePath)
-  {
+internal static class InternalResources {
+  public static Stream? GetStream(string relativePath) {
     var assembly = Assembly.GetExecutingAssembly();
     var resourceName = $"{assembly.GetName().Name}.{relativePath.Replace('/', '.')}";
     return assembly.GetManifestResourceStream(resourceName);
   }
 
-  public static byte[]? GetBytes(string relativePath)
-  {
+  public static byte[]? GetBytes(string relativePath) {
     using var stream = GetStream(relativePath);
     if (stream == null) return null;
 
@@ -21,8 +19,7 @@ internal static class InternalResources
     return ms.ToArray();
   }
 
-  public static string? GetText(string relativePath)
-  {
+  public static string? GetText(string relativePath) {
     using var stream = GetStream(relativePath);
     if (stream == null) return null;
 
@@ -34,17 +31,17 @@ internal static class InternalResources
 }
 
 
-public class Resources<T>() where T : struct, Enum
-{
-  public Stream? GetStream(T path)
-  {
+public class Resources<T>() where T : struct, Enum {
+  public Stream? GetStream(T path) {
     var assembly = typeof(T).Assembly;
-    var resourceName = $"{assembly.GetName().Name}.{path.ToString().Replace('/', '.')}";
+    var field = path.GetType().GetField(path.ToString());
+    var attribute = field?.GetCustomAttribute<DisplayAttribute>();
+    var name = attribute?.Name ?? path.ToString();
+    var resourceName = $"{assembly.GetName().Name}.{name.Replace('/', '.')}";
     return assembly.GetManifestResourceStream(resourceName);
   }
 
-  public byte[]? GetBytes(T path)
-  {
+  public byte[]? GetBytes(T path) {
     using var stream = GetStream(path);
     if (stream == null) return null;
 
@@ -53,8 +50,7 @@ public class Resources<T>() where T : struct, Enum
     return ms.ToArray();
   }
 
-  public string? GetText(T path)
-  {
+  public string? GetText(T path) {
     using var stream = GetStream(path);
     if (stream == null) return null;
 
