662a167b2e9aff7fb65a2dec071b24a3de680f3a
mrt <motifyee@gmail.com>
2025-12-30 16:42:21 +0200

    feat: Introduce SpireUtil for centralized PDF printing and saving, refactoring invoice printing to utilize it.

--- PrintingLibrary/SyncfusionUtils/SyncfusionSpreadsheetUtils.cs
+++ PrintingLibrary/SyncfusionUtils/SyncfusionSpreadsheetUtils.cs
@@ -2,22 +2,27 @@ using Microsoft.AspNetCore.Http;
 using Syncfusion.EJ2.Spreadsheet;
 using Syncfusion.Licensing;
 
-
 namespace PrintingLibrary.SyncfusionUtils;
 
-public static class SyncfusionSpreadsheetUtils {
+// TODO: optionally include syncfusion binaries
+public static class SyncfusionSpreadsheetUtils
+{
 
-  public static void RegisterLicence() {
+  public static void RegisterLicence()
+  {
     // Register Syncfusion license
     SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NHaF5cXmVCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWH1dcHRRRWBeUkB+XktWYUo=");
   }
 
-  public static string Open(IFormFile file) {
-    if (file == null || file.Length == 0) {
+  public static string Open(IFormFile file)
+  {
+    if (file == null || file.Length == 0)
+    {
       throw new ArgumentException("No file uploaded", nameof(file));
     }
 
-    OpenRequest open = new() {
+    OpenRequest open = new()
+    {
       File = file
     };
 
